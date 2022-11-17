e1bd71e6dc8a8124a17cf3a4c6b454ef3a105bfa
poka <khs0826@dsm.hs.kr>
2022-11-17 17:08:43 +0900

    cors

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"RGBContribution/contribution"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
 )
 
@@ -21,6 +22,11 @@ func one(c echo.Context) error {
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 
 	e.GET("/users/:name/:year", one)
 
